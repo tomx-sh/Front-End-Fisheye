3ccad9c71c1bfd8e98db8460e046afb1d1218eac
Thomas DOUCHE <Thomas.douche@aol.com>
2024-04-11 16:43:38 +0200

    aria labels

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -10,22 +10,26 @@ export default function photographerTemplate(data) {
 
     function getUserCardDOM() {
         // Create the DOM elements
-        const article = document.createElement('article');
-        const link = document.createElement('a');
-        link.setAttribute('href', `./photographer.html?id=${id}`);
-        const img = document.createElement('img');
-        const h2 = document.createElement('h2');
-        const address = document.createElement('address');
-        const cite = document.createElement('cite');
-        const priceEl = document.createElement('p');
-        priceEl.setAttribute('class', 'price');
-
+        const article   = document.createElement('article');
+        const link      = document.createElement('a');
+        const img       = document.createElement('img');
+        const h2        = document.createElement('h2');
+        const address   = document.createElement('address');
+        const cite      = document.createElement('cite');
+        const priceEl   = document.createElement('p');
+        
         // Set data to the DOM elements
-        img.setAttribute("src", picture)
+        link.setAttribute('href', `./photographer.html?id=${id}`);
+        link.setAttribute('aria-label', `Voir la page de ${name}`);
+        const h2Id = `name_${id}`;
+        h2.setAttribute('id', h2Id);
         h2.textContent = name;
+        img.setAttribute("src", picture)
+        img.setAttribute("aria-labelledby", h2Id);
         address.textContent = `${city}, ${country}`;
         cite.textContent = tagline;
         priceEl.textContent = `${price}€/jour`;
+        priceEl.setAttribute('class', 'price');
 
         // Assemble the DOM elements
         article.appendChild(link);
