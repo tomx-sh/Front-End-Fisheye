eee79ac5a9c615786057d995337b281dc767788b
Thomas DOUCHE <Thomas.douche@aol.com>
2024-04-11 18:01:55 +0200

    photographer page script

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,33 @@
-//Mettre le code JavaScript lié à la page photographer.html
+import { PhotographersApi } from "../api/api.js";
+
+
+async function init() {
+    // Get id from search params
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    // fetch data
+    const photographersApi = new PhotographersApi();
+    const photographer = await photographersApi.fetchPhotographerById(id);
+    const name = photographer.name;
+    const city = photographer.city;
+    const country = photographer.country;
+    const tagline = photographer.tagline;
+    const avatarFileName = photographer.portrait;
+
+    // Get DOM elements
+    const nameEl     = document.querySelector('#name');
+    const locationEl = document.querySelector('#location');
+    const taglineEl  = document.querySelector('#tagline');
+    const avatarEl   = document.querySelector('#avatar');
+
+    // Set data to DOM elements
+    nameEl.textContent = name;
+    locationEl.textContent = `${city}, ${country}`;
+    taglineEl.textContent = tagline;
+    avatarEl.src = `assets/photographers/Photographers_ID_Photos/${avatarFileName}`;
+    avatarEl.alt = name;
+    
+}
+
+init();
